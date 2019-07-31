2b9175e6397e209f966e4bb9a671a47c154b422c
freetoplayir <43073502+freetoplayir@users.noreply.github.com>
2019-07-31 08:45:45 +0430

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Clarifai configuration information
-const clarifaiKey = exports.clarifaiKey = 'YOUR CLARIFAI API KEY';
+const clarifaiKey = exports.clarifaiKey = 'f0bea1ba0a1740ed9ed68900fe72980d';
